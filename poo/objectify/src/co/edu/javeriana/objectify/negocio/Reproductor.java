4f053d9edfc574944b1a3334a2a466199af2389f
Javier Morales <moralesjm@gmail.com>
2016-07-09 17:00:41 -0500

    El reproductor funcionando con capacidad para reproducir de una cola, pausar y pasar a la siguiente cancion bien sea bajo comamdo o automaticamente al terminar la canción actual.

--- poo/objectify/src/co/edu/javeriana/objectify/negocio/Reproductor.java
+++ poo/objectify/src/co/edu/javeriana/objectify/negocio/Reproductor.java
@@ -1,18 +1,27 @@
 package co.edu.javeriana.objectify.negocio;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Reproductor {
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+@SuppressWarnings("serial")
+public class Reproductor extends JFXPanel {
 	
 	private static Reproductor instancia;	
 	private Queue<Cancion> colaReproduccion; 
+	private Cancion reproduciendo;
+	private MediaPlayer player;
 	
 	private Reproductor() {
-		colaReproduccion = new LinkedList<Cancion>();
+		colaReproduccion = new LinkedList<Cancion>();		
 	}
 	
-	public static Reproductor obtenerInstancia() {
+	public static synchronized Reproductor obtenerInstancia() {
 		if (instancia == null) {
 			instancia = new Reproductor();
 		}
@@ -27,13 +36,40 @@ public class Reproductor {
 		// Se acabo el papel higienico, por suerte no lo necesitamos
 		this.colaReproduccion.clear();
 	}
-	
-	public void reproducir() {
-		
+	public synchronized void reproducir() {
+		if (this.player != null) {
+			this.player.play();
+		} else {
+			try {
+				URL url;
+				this.reproduciendo = this.colaReproduccion.poll();
+				if (this.reproduciendo != null) {
+					url = new URL(this.reproduciendo.getRutaArchivo());
+			        Media media = new Media(url.toString());
+			        this.player = new MediaPlayer(media);
+			        this.player.setOnEndOfMedia(new Runnable() {						
+						@Override
+						public void run() {							
+							player = null;
+							Reproductor.obtenerInstancia().reproducir();
+						}
+					});
+			        this.player.play();
+				}
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} 
+		}
 	}
 	
+	public void siguiente() {
+		this.player.stop();
+		this.player = null;
+		reproducir();
+	}
+
 	public void pausar() {
-		
+		this.player.pause();
 	}
-	
+
 }
--- poo/objectify/src/co/edu/javeriana/objectify/presentacion/ObjectifyConsola.java
+++ poo/objectify/src/co/edu/javeriana/objectify/presentacion/ObjectifyConsola.java
@@ -1,6 +1,13 @@
 package co.edu.javeriana.objectify.presentacion;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import co.edu.javeriana.objectify.negocio.Cancion;
 import co.edu.javeriana.objectify.negocio.Objectify;
+import co.edu.javeriana.objectify.negocio.Reproductor;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * Clase de uso del sistema de música usando una interfaz por consola.
@@ -9,10 +16,32 @@ import co.edu.javeriana.objectify.negocio.Objectify;
  */
 public class ObjectifyConsola {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException, IOException{
 		Objectify negocio = new Objectify();
 		
-		System.out.println(negocio);
+		Reproductor reproductor = Reproductor.obtenerInstancia();
+		reproductor.agregarACola(new Cancion(null, "C1", 100, "http://ocrmirror.org/files/music/remixes/Legend_of_Zelda_Twilight_Princess_Wistful_OC_ReMix.mp3"));
+		reproductor.agregarACola(new Cancion(null, "C2", 100, "http://ocrmirror.org/files/music/remixes/Legend_of_Zelda_Ocarina_of_Time_Lullaby_of_the_Sky_OC_ReMix.mp3"));
+		reproductor.agregarACola(new Cancion(null, "C3", 100, "http://ocrmirror.org/files/music/remixes/Legend_of_Zelda_Twilight_Princess_Zelda%27s_Lament_OC_ReMix.mp3"));		
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		String comando = "p";
+		while (!comando.equals("q")){
+			if (comando.equals("p")) {
+				Reproductor.obtenerInstancia().reproducir();
+			}
+			if (comando.equals("s")) {
+				Reproductor.obtenerInstancia().pausar();
+			}
+			if (comando.equals("n")) {
+				Reproductor.obtenerInstancia().siguiente();
+			}
+			
+			comando = scanner.nextLine();
+		}
+		scanner.close();		
+		System.exit(0);		
 	}
 
 }
